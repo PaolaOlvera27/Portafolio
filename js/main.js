d094635aad332146a876b5ab94d16c1ffe40c7b4
PaolaOlvera27 <apao.iq1@gmail.com>
2016-11-22 14:17:31 -0600

    avance

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 $(document).ready(function(){
   //inicia funcion escribir texto
     var i = 0;
-    var texto ='kjkjjlkjldsfffffffffffffffffffffffffffffffffffffffjlkjdfkljgkljklgjdkljgkldfjglkdfjgkldfjgkdfgkldjfgkdfjk';
+    var texto ='';
         function escribirTexto() {
           $('#about-me').append(texto[i]);
           if(i<texto.length){
@@ -21,3 +21,6 @@ $(document).ready(function(){
 $(document).ready(function(){
    $('ul.tabs').tabs('select_tab', 'tab_id');
  });
+ $(document).ready(function(){
+      $('.carousel').carousel();
+    });
