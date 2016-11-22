8f8984a6f4d7b474728366f7f1c41e8262a1dbe5
PaolaOlvera27 <apao.iq1@gmail.com>
2016-11-21 22:02:30 -0600

    primer avance

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $('.button-collapse').sideNav();
+});
+$(document).ready(function(){
+    $('.parallax').parallax();
+});
+$(document).ready(function(){
+  //inicia funcion escribir texto
+    var i = 0;
+    var texto ='kjkjjlkjldsfffffffffffffffffffffffffffffffffffffffjlkjdfkljgkljklgjdkljgkldfjglkdfjgkldfjgkdfgkldjfgkdfjk';
+        function escribirTexto() {
+          $('#about-me').append(texto[i]);
+          if(i<texto.length){
+            i++;
+            setTimeout(escribirTexto, 250);
+          }
+        };
+        escribirTexto();
+  //finaliza funcion escribir texto
+});
+$(document).ready(function(){
+   $('ul.tabs').tabs('select_tab', 'tab_id');
+ });
