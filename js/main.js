28610ce358e0103cf03e6aebf2f3255458222d52
PaolaOlvera27 <apao.iq1@gmail.com>
2016-11-23 11:31:00 -0600

    avance

--- js/main.js
+++ js/main.js
@@ -1,26 +1,20 @@
 $(document).ready(function(){
     $('.button-collapse').sideNav();
-});
-$(document).ready(function(){
     $('.parallax').parallax();
+    $('ul.tabs').tabs('select_tab', 'tab_id');
+    $('.carousel').carousel();
 });
-$(document).ready(function(){
+
+
   //inicia funcion escribir texto
-    var i = 0;
-    var texto ='';
-        function escribirTexto() {
-          $('#about-me').append(texto[i]);
-          if(i<texto.length){
-            i++;
-            setTimeout(escribirTexto, 250);
-          }
-        };
-        escribirTexto();
-  //finaliza funcion escribir texto
-});
-$(document).ready(function(){
-   $('ul.tabs').tabs('select_tab', 'tab_id');
- });
- $(document).ready(function(){
-      $('.carousel').carousel();
-    });
+  //   var i = 0;
+  //   var texto ='';
+  //       function escribirTexto() {
+  //         $('#about-me').append(texto[i]);
+  //         if(i<texto.length){
+  //           i++;
+  //           setTimeout(escribirTexto, 250);
+  //         }
+  //       };
+  //       escribirTexto();
+  // //finaliza funcion escribir texto
