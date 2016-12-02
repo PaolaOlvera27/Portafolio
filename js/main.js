bde100152c6953728a4ef47c015b196d74ac3473
PaolaOlvera27 <apao.iq1@gmail.com>
2016-12-02 01:13:44 -0600

    correcciones

--- js/main.js
+++ js/main.js
@@ -1,52 +1,11 @@
 $(document).ready(function(){
-    $('.button-collapse').sideNav();
-    $('.button-collapse').sideNav('hide');
+    $('.carousel.carousel-slider').carousel({full_width: true});
+    $('.tooltipped').tooltip({delay: 50});
     $('.parallax').parallax();
     $('ul.tabs').tabs('select_tab', 'tab_id');
     $('.carousel').carousel();
-     $('.materialboxed').materialbox();
-//
-var value = 0;
-var pos = 0;
-var progressHidden = false;
-var delay = 40;
-var progressEl = $('progress');
-
-// set interval
-var timer = setInterval(progress, 100);
-
-function progress() {
-
-  // run counter
-  value++;
-  if (value < 99) {
-    progressEl.val(value);
-    pos = 1 - (value/100);
-  } else if(value < (delay + 100) ) {
-    progressEl.val(99);
-    pos = 0;
-  } else {
-    value = 0;
-  }
-
-  // update background
-  progressEl.css('background-position', '0 '+ pos +'em');
-
-  // show/hide progress
-  if(!progressHidden && value >= 100) {
-    progressEl.addClass("hidden");
-    progressHidden = true;
-
-  } else if(progressHidden && value < 100) {
-    progressEl.val(0);
-    progressEl.removeClass("hidden");
-    progressHidden = false;
-  }
-
-}
-///
+    $('.slider').slider({full_width: true});
 });
-
 
   //inicia funcion escribir texto
   //   var i = 0;
